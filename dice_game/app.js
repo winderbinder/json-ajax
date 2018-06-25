d1e97dcea91e36ff8e08beffab23cd66788ff4f1
winderbinder <junior_19000@hotmail.com>
2018-06-25 10:35:27 -0600

    added javascript logic that I have so far

---
+++ dice_game/app.js
@@ -0,0 +1,66 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+let scores, roundScore, activePlayer;
+
+scores = [0, 0]
+roundScore = 0;
+activePlayer = 0;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+  // 1. random number
+let dice = Math.floor(Math.random() * 6) + 1;
+
+// 2. display the result
+let diceDOM = document.querySelector('.dice');
+diceDOM.style.display = 'block';
+diceDOM.src = 'dice-' + dice + '.png';
+
+
+// 3. update the round score if the rolled number is not a 1
+  if (dice !== 1) {
+
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore
+  } else {
+
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    // document.querySelector('.player-0-panel').classList.remove('active');
+    // document.querySelector('.player-1-panel').classList.add('active')
+
+  }
+
+});
+
+
+
+
+// document.querySelector('#score-' + activePlayer).textContent = dice;
+// document.querySelector('#score-' + activePlayer).innerHTML = `<em> ${dice} </em>;
+
+// let x = document.querySelector('#score-0').textContent;
